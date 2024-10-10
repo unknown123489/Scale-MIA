d7845fff7a6fb172d2653e1821f9600677b2995e
unknown123489 <3254372595@qq.com>
2024-10-09 22:01:32 -0400

    Update targeted-adv-train.py

--- fedavg-cifar/targeted-adv-train.py
+++ fedavg-cifar/targeted-adv-train.py
@@ -22,7 +22,7 @@ from nets.CNNencoder import Encoder
 from nets.CNNdecoder import Decoder
 from torchvision.datasets import CIFAR10
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(device)
 
